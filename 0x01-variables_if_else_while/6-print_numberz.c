3df90e7d95cf14f22df61b16f4abaf2de5ab2c3c
Amos Kinuthia <amoskinuthia15@gmail.com>
2022-09-10 08:47:26 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,11 @@
 */
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i < 10; i++)
-	{
-		printf("%i", i);
-	}
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
 	putchar('\n');
 
 	return (0);
