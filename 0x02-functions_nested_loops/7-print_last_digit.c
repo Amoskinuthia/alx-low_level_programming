d9a3b937c994b7cedd325b23ff2df26f6a775b7b
Amos Kinuthia <amoskinuthia15@gmail.com>
2022-09-14 00:34:31 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,15 @@
 *@n: int type number
 * Return: the last  value of @n
 */
-int _print_last_digit(int)
+int _print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
 	if (n < 0)
-
+	{
 	last_digit *= -1;
 	_putchar(last_digit + '0');
+	}
 }
+
 
